a0e38c92ea51b9f699f58450f49224ae554fb7c8
Rakesh <rakesh.chavan776@gmail.com>
2022-12-15 21:41:05 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './my/header/header.component';
+import { LastFewTransactionComponent } from './my/last-few-transaction/last-few-transaction.component';
+import { MainComponent } from './my/main/main.component';
+import { SalesByCategoryComponent } from './my/sales-by-category/sales-by-category.component';
+import { SideNavComponent } from './my/side-nav/side-nav.component';
+import { TopThreeProductsComponent } from './my/top-three-products/top-three-products.component';
+import { TopWidgetsComponent } from './my/top-widgets/top-widgets.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SalesByMonthComponent } from './my/sales-by-month/sales-by-month.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LastFewTransactionComponent,
+    MainComponent,
+    SalesByCategoryComponent,
+    SideNavComponent,
+    TopThreeProductsComponent,
+    TopWidgetsComponent,
+    SalesByMonthComponent,
+  
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
